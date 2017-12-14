f6fe9dd8786d3e0f151e319cdbf6ccb63d43a78c
Juan Zamudio <jzamudio14@gmail.com>
2017-12-13 17:08:06 -0800

    First commit

---
+++ src/lp1/GraphVisualizer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lp1;
+
+/**
+ * @author jzamudio
+ *
+ */
+public class GraphVisualizer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
